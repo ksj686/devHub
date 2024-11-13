ce26f768a9c43c0cfbe815834acc2169fd115d8b
ksj686 <kim_se_joon@naver.com>
2024-11-13 17:07:32 +0900

    1차 프로젝트

---
+++ src/main/java/app/labs/DevHubApplication.java
@@ -0,0 +1,13 @@
+package app.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevHubApplication.class, args);
+	}
+
+}
