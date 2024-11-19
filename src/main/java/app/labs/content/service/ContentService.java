89481a39c7478ec89b44fc2f99158c2062a3371a
ChaSungWook <0805nick@gmail.com>
2024-11-19 17:05:31 +0900

    일반 게시판 글 작성 / 조회 기능 추가

--- src/main/java/app/labs/content/service/ContentService.java
+++ src/main/java/app/labs/content/service/ContentService.java
@@ -8,6 +8,8 @@ public interface ContentService {
 	Content getContentInfo(int contentId); // 글 내용 조회
 	void createContent(Content content); // 글 작성
 	void editContent(Content content); // 글 수정
-	void deleteContent(int contentId); // 글 삭제
+	void deleteContent(int contentId, String userId); // 글 삭제
 	List<Content>getAllContents(); //모든 글 조회
+//	void increaseRecommend(int contentId); //추천수 증가
+//	int getRecommendCount(int contentId); //추천수 조회
 }	
