866b2594c4add2542dec0e155e8068c25e061cf3
ksj686 <kim_se_joon@naver.com>
2024-11-20 17:36:38 +0900

    메인화면 1차 기본틀

--- src/main/java/app/labs/HomeController.java
+++ src/main/java/app/labs/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	@GetMapping("/")
 	public String index() {
-		 return "home";
+		 return "thymeleaf/main_content";
 	 }
 	
 	@GetMapping("/content")
